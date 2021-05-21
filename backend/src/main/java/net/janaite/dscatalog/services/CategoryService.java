43b3eaf889b82597a4742d1b0680820e09ceaf60
janaite <janaite@gmail.com>
2021-05-20 22:46:55 -0300

    DELETE a Category

--- backend/src/main/java/net/janaite/dscatalog/services/CategoryService.java
+++ backend/src/main/java/net/janaite/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import net.janaite.dscatalog.dto.CategoryDTO;
 import net.janaite.dscatalog.entities.Category;
 import net.janaite.dscatalog.repositories.CategoryRepository;
+import net.janaite.dscatalog.services.exceptions.DatabaseException;
 import net.janaite.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -48,10 +51,22 @@ public class CategoryService {
 			Category entity = repository.getOne(id); // getOne do not access database until you save the entity
 			entity.setName(dto.getName());
 			entity = repository.save(entity);
-			
+
 			return new CategoryDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException(String.format("ID not found [%d]", id));
 		}
+	}
+
+	// dont use @Transcation to capture a exception
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(String.format("ID not found [%d]", id));
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integriry Violation");
+		}
+
 	}
 }
