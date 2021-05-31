089cd4d13900a1526c7e882f53eee057f00cd767
janaite <janaite@gmail.com>
2021-05-31 14:53:09 -0300

    Category and Product bidirectional mapping

--- backend/src/main/java/net/janaite/dscatalog/entities/Category.java
+++ backend/src/main/java/net/janaite/dscatalog/entities/Category.java
@@ -2,12 +2,15 @@ package net.janaite.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -27,6 +30,10 @@ public class Category implements Serializable {
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
+	
 	
 	public Category() {
 	}
@@ -51,6 +58,10 @@ public class Category implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public void setProducts(Set<Product> products) {
+		this.products = products;
 	}
 
 	@Override
