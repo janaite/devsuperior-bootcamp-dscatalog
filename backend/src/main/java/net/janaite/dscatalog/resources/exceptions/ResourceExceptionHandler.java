6bb483000acd3d8f7ecb24675edead4bc30a05e8
janaite <janaite@gmail.com>
2021-06-01 18:38:37 -0300

    custom validation response

--- backend/src/main/java/net/janaite/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/net/janaite/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -38,6 +39,25 @@ public class ResourceExceptionHandler {
 		err.setStatus(status.value());
 		err.setError("Database exception");
 		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		final HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;	// code 422
+		
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation exception");
+		err.setMessage(e.getMessage());
+
+		e.getBindingResult().getFieldErrors().forEach(fe -> {
+			err.addError(fe.getField(), fe.getDefaultMessage());
+		});
+		
 		err.setPath(request.getRequestURI());
 		
 		return ResponseEntity.status(status).body(err);
