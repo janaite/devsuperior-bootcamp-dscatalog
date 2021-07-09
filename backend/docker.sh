d6ecf507574afcc73e6ef9389bf9eb4f1dd21b05
janaite <janaite@gmail.com>
2021-07-08 21:44:31 -0300

    Dev profile working (Docker for test db)

---
+++ backend/docker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# JANAITE 20210708
+
+docker build -t janaite/devsuperior-dscatalog .
+
+docker rm -f devsup_sqlqueires
+
+docker run -d \
+   --name devsup_dscatalog \
+   -p 5432:5432 \
+   janaite/devsuperior-dscatalog
