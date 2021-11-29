13e70d30e0d1abfc7493e31efb9aeb57118b4280
janaite <janaite@gmail.com>
2021-11-29 17:18:35 -0300

    ProductDetails using axios

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,23 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Product } from 'types/product';
+import { BASE_URL } from 'util/requests';
 
 import './styles.css';
 
 const ProductDetails = () => {
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    // call once
+    axios.get(BASE_URL + '/products/2').then((response) => {
+      setProduct(response.data);
+    });
+  }, []);
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
@@ -17,23 +30,17 @@ const ProductDetails = () => {
         <div className="row">
           <div className="col-xl-6">
             <div className="img-container">
-              <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-                alt="Nome do Produto"
-              />
+              <img src={product?.imgUrl} alt={product?.name} />
             </div>
             <div className="name-price-container">
-              <h1>Nome do Produto</h1>
-              <ProductPrice price={12345.67} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do Produto</h2>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Inventore, laborum.
-              </p>
+              <p>{product?.description}</p>
             </div>
           </div>
         </div>
