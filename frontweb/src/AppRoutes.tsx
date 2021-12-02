f64f614e6c325ad9948ce646057d4f1f9190dd92
janaite <janaite@gmail.com>
2021-12-02 11:08:25 -0300

    Auth routes

--- frontweb/src/AppRoutes.tsx
+++ frontweb/src/AppRoutes.tsx
@@ -2,6 +2,7 @@ import Navbar from 'components/Navbar';
 import Home from 'pages/Home';
 import Catalog from 'pages/Catalog';
 import Admin from 'pages/Admin';
+import Auth from 'pages/Admin/Auth';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ProductDetails from 'pages/ProductDetails';
 
@@ -12,6 +13,11 @@ const AppRoutes = () => (
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Catalog />} />
       <Route path="/products/:productId" element={<ProductDetails />} />
+      <Route
+        path="/admin/auth"
+        element={<Navigate replace to="/admin/auth/login" />}
+      />
+      <Route path="/admin/auth/*" element={<Auth />} />
       <Route
         path="/admin"
         element={<Navigate replace to="/admin/products" />}
